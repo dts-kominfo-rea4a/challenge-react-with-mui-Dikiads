1a44271e8e7e3c51a49343bee39e91c3c8b9f881
Diki Darussalam <dadsnaho@gmail.com>
2022-09-27 22:52:06 +0700

    coba3

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -36,10 +36,10 @@ const ContactForm = ({fnnewContact}) => {
         <Card sx={{ width: '70%', borderRadius: '10px', boxShadow: '0 0 7px rgb(0,0,0,0.1)'}}>
             <CardContent>
                 <form style={{paddingLeft: '2.5em'}} onSubmit={submitFormHandler}>    
-                    <TextField id="filled-basic" label="Name" variant="filled" sx={{width: '100%', height: '20%'}}  value={newContact.name} onChange={ e => inputHandler('name', e.target.value)} required/><br/><br/>
-                    <TextField id="filled-basic" label="Phone" variant="filled" sx={{width: '100%'}}  value={newContact.phone} onChange={e => inputHandler('phone', e.target.value)} type='number' required/><br/><br/>
-                    <TextField id="filled-basic" label="Email" variant="filled" sx={{width: '100%'}}  value={newContact.email} onChange={e => inputHandler('email', e.target.value)} type='email' required/><br/><br/>
-                    <TextField id="filled-basic" label="Photo URL" variant="filled" sx={{width: '100%'}}  value={newContact.photo} onChange={e => inputHandler('photo', e.target.value)}  required/><br/>
+                    <TextField id="filled-basic" label="Name" variant="filled" sx={{width: '100%', height: '20%'}}  value={newContact.name} onChange={ e => inputHandler('name', e.target.value)} name='name' required/><br/><br/>
+                    <TextField id="filled-basic" label="Phone" variant="filled" sx={{width: '100%'}}  value={newContact.phone} onChange={e => inputHandler('phone', e.target.value)} name="phone" required/><br/><br/>
+                    <TextField id="filled-basic" label="Email" variant="filled" sx={{width: '100%'}}  value={newContact.email} onChange={e => inputHandler('email', e.target.value)} name='email' required/><br/><br/>
+                    <TextField id="filled-basic" label="Photo URL" variant="filled" sx={{width: '100%'}}  value={newContact.photo} onChange={e => inputHandler('photo', e.target.value)}  name='photo' required/><br/>
                     <CardActions >
                         <Button type='submit' variant="outlined" startIcon={<PersonAddIcon />} sx={{margin: '0.5em auto'}}>Add New</Button>
                     </CardActions>
